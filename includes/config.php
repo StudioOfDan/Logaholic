f46ccb1fc798c6a722131f3224c78479feb92895
stijnana <stijn.olthof@live.nl>
2016-02-03 14:54:40 +0100

    basis opstelling van ontwikkel omgeving

---
+++ includes/config.php
@@ -0,0 +1,19 @@
+    <?php
+// In dit bestand vind je alle instellingen, zoals databasegegevens.
+
+// Voor de veiligheid zou dit bestand boven de webroot geplaatst moeten worden. 
+
+// Definieer de status van het project
+define('PROJECT_STATUS','development');
+
+// Stel de instellingen in op Nederlands
+setlocale(LC_ALL, 'nl_NL');
+
+// databasegegevens
+        define('DB_HOST','localhost');
+        define('DB_NAME','overdeschutting');
+        define('DB_USERNAME','root');
+        define('DB_PASSWORD','');
+
+
+?>
